3e26dfeb6afd018540fc9a24e86d1510070fccb7
sos1819ajm <alberto_jimenez_1996@hotmail.com>
2019-03-19 22:07:39 +0000

    ok

--- index.js
+++ index.js
@@ -8,36 +8,27 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-
-
 //////// ANTONIO ESCOBAR NÚÑEZ//////////
 
 var pollutionStats = [{
-    
     country: "spain",
     year: "2017",
     pollution_tco2: "282.364",
     pollution_kg1000: "0.18",
     pollution_perca: "6.09"
-    
 },{
-    
     country: "spain",
     year: "2016",
     pollution_tco2: "263.908",
     pollution_kg1000: "0.17",
     pollution_perca: "5.69"
-
 },{
-    
     country: "spain",
     year: "2015",
     pollution_tco2: "271.171",
     pollution_kg1000: "0.18",
     pollution_perca: "5.84"
-    
 },{
-    
     country: "alemania",
     year: "2017",
     pollution_tco2: "796.0529",
@@ -45,99 +36,75 @@ var pollutionStats = [{
     pollution_perca: "9.71"
     
 },{
-    
     country: "alemania",
     year: "2016",
     pollution_tco2: "798.582",
     pollution_kg1000: "0.22",
     pollution_perca: "9.75"  
-
 },{
-    
     country: "alemania",
     year: "2015",
     pollution_tco2: "789.898",
     pollution_kg1000: "0.22",
     pollution_perca: "9.67" 
-
 },{
-    
     country: "reino unido",
     year: "2017",
     pollution_tco2: "379.15",
     pollution_kg1000: "0.22",
     pollution_perca: "9.67" 
-
 },{
-    
     country: "reino unido",
     year: "2016",
     pollution_tco2: "391.472",
     pollution_kg1000: "0.15",
     pollution_perca: "5.95" 
-
 },{
-    
     country: "reino unido",
     year: "2015",
     pollution_tco2: "416.749",
     pollution_kg1000: "0.16",
     pollution_perca: "6.37" 
-
 },{
-    
     country: "francia",
     year: "2017",
     pollution_tco2: "338.193",
     pollution_kg1000: "0.13",
     pollution_perca: "5.2" 
-    
 },{
-    
     country: "francia",
     year: "2016",
     pollution_tco2: "332.034",
     pollution_kg1000: "0.13",
     pollution_perca: "5.13" 
-
 },{
-    
     country: "francia",
     year: "2015",
     pollution_tco2: "327.725",
     pollution_kg1000: "0.13",
     pollution_perca: "5.08" 
-
 },{
-    
     country: "italia",
     year: "2017",
     pollution_tco2: "361.193",
     pollution_kg1000: "0.17",
     pollution_perca: "6.08" 
-
 },{
-    
     country: "italia",
     year: "2016",
     pollution_tco2: "356.461",
     pollution_kg1000: "0.17",
     pollution_perca: "6" 
-
 },{
-    
     country: "italia",
     year: "2015",
     pollution_tco2: "354.355",
     pollution_kg1000: "0.17",
     pollution_perca: "5.96" 
-    
 }];
-
 
 //GET /api/v1/pollutionStats/loadInitialData
 app.get("/api/v1/pollutionStats/loadInitialData", (req, res) => {
-        
     pollutionStats.push({
         country: "italia",
         year: "2015",
@@ -159,10 +126,8 @@ app.get("/api/v1/pollutionStats/loadInitialData", (req, res) => {
         pollution_kg1000: "0.19",
         pollution_perca: "6.08"
     });
-        
     res.sendStatus(201);
 });
-
 
 /// GET /api/v1/pollutionStats ///
 
@@ -174,11 +139,8 @@ app.get("/api/v1/pollutionStats",(req,res)=>{
 
 app.post("/api/v1/pollutionStats", (req,res)=>{
     var newPollutionStats = req.body;
-    
     pollutionStats.push(newPollutionStats)
-    
     res.sendStatus(201);
-    
 });
 
 // GET /api/v1/pollutionStats/country
@@ -205,36 +167,27 @@ app.delete("/api/v1/pollutionStats",(req,res) =>{
 /// GET /api/v1/pollutionStats/:country/:year ///
 
 app.get("/api/v1/pollutionStats/:country/:year",(req,res)=>{
-    
     var country = req.params.country;
     var year = req.params.year;
- 
-    
     var filteredPollutionStats = pollutionStats.filter((c)=>{
         return c.country == country && c.year == year ;
-      
     })
     if(filteredPollutionStats.length >=1){
         res.send(filteredPollutionStats[0]);
     } else{
         res.sendStatus(404);
     }
-   
     res.sendStatus(200);
 }); 
 
 /// PUT ///
 
-
 app.put("/api/v1/pollutionStats/:country/:year",(req,res)=>{
-    
     var country = req.params.country;
     var year = req.params.year;
     var updatePollutionStats = req.body;
     var found = false;
-    
     var updatePollutionStats = pollutionStats.map((c)=>{
-        
         if(c.country == country && c.year == year){
             found = true;
             return updatePollutionStats;
@@ -242,15 +195,11 @@ app.put("/api/v1/pollutionStats/:country/:year",(req,res)=>{
             return c; 
         }
     })
-    
-        
-    
     if(found == false){
         res.sendStatus(404);
     }else{
         pollutionStats = updatePollutionStats;
     }
-   
     res.sendStatus(200);
 });
 
@@ -271,12 +220,9 @@ app.put("/api/v1/pollutionStats", (req, res) => {
 /// DELETE  concreto///
 
 app.delete("/api/v1/pollutionStats/:country/:year",(req,res)=>{
-    
     var country = req.params.country;
     var year = req.params.year;
-    
     var found = false;
-    
     var updatePollutionStats = pollutionStats.filter((c)=>{
         if(c.country != country && c.year != year){
             found = true;
@@ -289,32 +235,23 @@ app.delete("/api/v1/pollutionStats/:country/:year",(req,res)=>{
         pollutionStats = updatePollutionStats;
         res.sendStatus(200); 
     }
-    
-    
-        
-    
     if(found == false){
         res.sendStatus(404);
     }else{
         pollutionStats = updatePollutionStats;
     }
-   
     res.sendStatus(200);
 });
 
 ///////////////////////////////////////
 ///////////////////////////////////////
-///////////////////////////////////////
-//////////////////////////////////////
-//////////////////////////////////////
 
-//API REST life_expectancy_stats
+//                                              API REST life_expectancy_stats
 
 var life_expectancy_stats = [];
 
 //GET /api/v1/life-expectancy-stats/loadInitialData
 app.get("/api/v1/life-expectancy-stats/loadInitialData", (req, res) => {
-        
     life_expectancy_stats.push({
         country: "spain",
         year: "2015",
@@ -336,7 +273,6 @@ app.get("/api/v1/life-expectancy-stats/loadInitialData", (req, res) => {
         expectancy_man: "80.6",
         expectancy: "83.4"
     });
-        
     res.sendStatus(201);
 });
 
