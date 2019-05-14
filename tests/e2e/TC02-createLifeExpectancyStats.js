0304f24ac07b332293807fcfad8e103612cab0b8
sos1819ajm <alberto_jimenez_1996@hotmail.com>
2019-05-14 18:47:22 +0000

    ok

---
+++ tests/e2e/TC02-createLifeExpectancyStats.js
@@ -0,0 +1,22 @@
+describe("Check if a new stat can be created: ", function(){
+        
+        it("List should grow after the stat creation", function(){
+                
+            browser.get("https://sos1819-12.herokuapp.com/ui/v1/life-expectancy-stats/#!/");
+                
+            element.all(by.repeater("stat in life_expectancy_stats")).then(function(initialStats){
+         
+                element(by.model("newStat.country")).sendKeys("paisPrueba");
+                element(by.model("newStat.year")).sendKeys(2999);
+                element(by.model("newStat.expectancy_woman")).sendKeys(999);
+                element(by.model("newStat.expectancy_man")).sendKeys(999);
+                element(by.model("newStat.expectancy")).sendKeys(999);
+                element(by.css('[value="add"]')).click();
+                element.all(by.repeater("stat in life_expectancy_stats")).then(function(finalStats){
+                    expect(finalStats.length).toEqual(initialStats.length+1);
+                });
+                                
+            });
+                
+        });
+});
--- tests/tests.js
+++ tests/tests.js
@@ -3,12 +3,13 @@ exports.config = {
     seleniumAddress: "http://localhost:4444/wd/hub",
     chromeOnly: true,
     specs: [
-                "e2e/TC01-loadPollutionStats.js",
+                //"e2e/TC01-loadPollutionStats.js",
 
-                "e2e/TC02-createPollutionStats.js",
+                //"e2e/TC02-createPollutionStats.js",
 
-                "e2e/TC01-loadYouthUnemploymentStats.js",
+                //"e2e/TC01-loadYouthUnemploymentStats.js",
                 
-                "e2e/TC01-loadLifeExpectancyStats.js"]
+                "e2e/TC01-loadLifeExpectancyStats.js",
+                "e2e/TC02-createLifeExpectancyStats.js"]
                 
 };
