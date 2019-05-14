e3937e72bd72bd6785bd56e000fa9196529a7733
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 14:24:02 +0000

    bien

--- tests/tests.js
+++ tests/tests.js
@@ -4,11 +4,10 @@ exports.config = {
     chromeOnly: true,
     specs: [
                 "e2e/TC01-loadPollutionStats.js",
-<<<<<<< HEAD
-                "e2e/TC02-createPollutionStats.js"]
-=======
-                "e2e/TC01-createPollutionStats.js",
-                "e2e/TC01-loadPollutionStats.js"]
->>>>>>> e44a3986f0df3891697b077b3fd7a96729b6e18f
+
+                "e2e/TC02-createPollutionStats.js",
+
+                "e2e/TC01-loadYouthUnemploymentStats.js"]
+
     
 };
