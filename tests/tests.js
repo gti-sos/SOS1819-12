8725cf2e2f917923fe3fb712245ae189d84a5226
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 23:00:15 +0000

    bien

--- tests/tests.js
+++ tests/tests.js
@@ -8,11 +8,11 @@ exports.config = {
 //                "e2e/TC01-loadLifeExpectaSncyStats.js",
                 
                 
-                "e2e/TC02-createPollutionStats.js",
+                  "e2e/TC02-createPollutionStats.js",
 //                "e2e/TC02-createLifeExpectancyStats.js",
 //                "e2e/TC02-createYouthUnemploymentStats.js",
                 
-//                "e2e/TC03-removePollutionStats.js",
+                  "e2e/TC03-removePollutionStats.js",
 //                "e2e/TC03-removeYouthUnemploymentStats.js",
 //                "e2e/TC03-removeLifeExpectancyStats.js"
                 ]
