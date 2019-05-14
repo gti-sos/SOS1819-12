cfd6867ca5843ce05f84db53d9b80f6fecca2d7a
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 14:02:16 +0000

    bien

--- tests/e2e/TC01-createPollutionStats.js
+++ tests/e2e/TC01-createPollutionStats.js
@@ -4,7 +4,7 @@ describe("Check if a new stat can be created: ", function(){
                 
                 browser.get("https://sos1819-12.herokuapp.com/ui/v1/pollution-stats/#!/");
                 
-                var initialStats = element.all(by.repeater("stat in pollutionStats"))
+                element.all(by.repeater("stat in pollutionStats"))
                     .then(function(initialStats){
                 
                         element(by.model("newStat.country")).sendkeys("testprueba");
@@ -12,7 +12,11 @@ describe("Check if a new stat can be created: ", function(){
                         element(by.model("newStat.pollution_tco2")).sendkeys(282.364);
                         element(by.model("newStat.pollution_kg1000")).sendkeys(0.18);
                         element(by.model("newStat.pollution_perca")).sendkeys(6.09);
-                        element(bs.css('[value="Añadir:"]')).click();
+                        element(bs.css('[value="add"]')).click();
+                        element.all(by.repeater("stat in pollutionStats"))
+                            .then(function(finalStats){
+                                expect(finalStats.length).toEqual(initialStats.length+1);
+                            });
                 });
                 
                 
