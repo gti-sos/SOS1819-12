2d37f50d7ec394b109c1dc92b914a464377cedfa
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 14:58:19 +0000

    bien

--- tests/e2e/TC02-createPollutionStats.js
+++ tests/e2e/TC02-createPollutionStats.js
@@ -4,20 +4,31 @@ describe("Check if a new stat can be created: ", function(){
                 
                 browser.get("https://sos1819-12.herokuapp.com/ui/v1/pollution-stats/#!/");
                 
+                check();
+                
+                function check(){
+                
                 element.all(by.repeater("stat in pollutionStats"))
                     .then(function(initialStats){
                 
-                        element(by.model("newStat.country")).sendKeys("testprueba");
-                        element(by.model("newStat.year")).sendKeys(8888);
-                        element(by.model("newStat.pollution_tco2")).sendKeys(282.364);
-                        element(by.model("newStat.pollution_kg1000")).sendKeys(0.18);
-                        element(by.model("newStat.pollution_perca")).sendKeys(6.09);
-                        element(by.css('[value="Añadir:"]')).click();
-                        element.all(by.repeater("stat in pollutionStats"))
-                            .then(function(finalStats){
-                                expect(finalStats.length).toEqual(initialStats.length+1);
-                            });
-                });
+                        if(initialStats.length==6){
+                                    
+                                element(by.css('[value="Siguiente"]')).click().then(check());
+                                    
+                                } else {
+                
+                
+                                element(by.model("newStat.country")).sendKeys("testprueba");
+                                element(by.model("newStat.year")).sendKeys(8888);
+                                element(by.model("newStat.pollution_tco2")).sendKeys(282.364);
+                                element(by.model("newStat.pollution_kg1000")).sendKeys(0.18);
+                                element(by.model("newStat.pollution_perca")).sendKeys(6.09);
+                                element(by.css('[value="Añadir:"]')).click();
+                                element.all(by.repeater("stat in pollutionStats"))
+                                    .then(function(finalStats){
+                                        expect(finalStats.length).toEqual(initialStats.length+1);
+                                    })};
+                })};
                 
                 
         });
