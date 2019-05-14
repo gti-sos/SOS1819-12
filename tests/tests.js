335275782672b164421d5925fba0990ad5da803c
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 22:53:35 +0000

    bien

--- tests/tests.js
+++ tests/tests.js
@@ -4,24 +4,17 @@ exports.config = {
     chromeOnly: true,
     specs: [
                 "e2e/TC01-loadPollutionStats.js",
+                "e2e/TC01-loadYouthUnemploymentStats.js",
+                "e2e/TC01-loadLifeExpectaSncyStats.js",
+                
+                
+                "e2e/TC02-createPollutionStats.js",
+                "e2e/TC02-createLifeExpectancyStats.js",
+                "e2e/TC02-createYouthUnemploymentStats.js",
+                
+                "e2e/TC03-removePollutionStats.js",
+                "e2e/TC03-removeYouthUnemploymentStats.js",
+                "e2e/TC03-removeLifeExpectancyStats.js"]
 
-                //"e2e/TC02-createPollutionStats.js",
-                "e2e/TC03-removePollutionStats.js"]
-
-                //"e2e/TC01-loadYouthUnemploymentStats.js",
-                
-                //"e2e/TC01-loadLifeExpectaSncyStats.js",
-                
-                //"e2e/TC02-createLifeExpectancyStats.js",
-                
-<<<<<<< HEAD
-                "e2e/TC02-removeLifeExpectancyStats.js",
-                
-                "TC02-createYouthUnemploymentStats.js",
-                
-                "e2e/TC03-removeYouthUnemploymentStats.js"]
-=======
-                //"e2e/TC02-removeLifeExpectancyStats.js"]
->>>>>>> 89aa58bab9f062c08d944c119b51dfdbc84fb190
                 
 };
