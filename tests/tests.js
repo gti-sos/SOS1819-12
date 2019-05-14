83711a741e308d89e3f544aff085c066783b7cef
sos1819aen <antonioescobar070@gmail.com>
2019-05-14 22:47:42 +0000

    bien

--- tests/tests.js
+++ tests/tests.js
@@ -5,7 +5,7 @@ exports.config = {
     specs: [
                 "e2e/TC01-loadPollutionStats.js",
 
-                "e2e/TC02-createPollutionStats.js",
+                //"e2e/TC02-createPollutionStats.js",
                 "e2e/TC03-removePollutionStats.js"]
 
                 //"e2e/TC01-loadYouthUnemploymentStats.js",
