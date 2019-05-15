011dba4b5fa52b45962fed081ebfef58af46d9f5
Andres Fernandez <andresfernandezgomez@gmail.com>
2019-05-15 07:12:48 +0000

    jjjj

--- public/SOS1819-12-app.js
+++ public/SOS1819-12-app.js
@@ -10,6 +10,12 @@ angular.module("SOS181912App",["ngRoute"])
             }).when("/ui/v1/life-expectancy-stats/edit/:country/:year",{
                 controller: "EditLESCtrl",
                 templateUrl: "frontend/v1/edit-LES.html"
+            }).when("/ui/v1/youth-unemployment-stats",{
+                controller: "ListyCtrl",
+                templateUrl: "youthApp/v1/listy.html"
+            }).when("/ui/v1/youth-unemployment-stats/edit/:country/:year",{
+                controller: "EdityCtrl",
+                templateUrl: "youthApp/v1/edity.html"
             });
     });
 console.log("SOS1819-12 App initialized!");
--- tests/tests.js
+++ tests/tests.js
@@ -3,17 +3,17 @@ exports.config = {
     seleniumAddress: "http://localhost:4444/wd/hub",
     chromeOnly: true,
     specs: [
-                "e2e/TC01-loadLifeExpectancyStats.js",
+               "e2e/TC01-loadLifeExpectancyStats.js",
                 "e2e/TC02-createLifeExpectancyStats.js",
                 "e2e/TC03-removeLifeExpectancyStats.js",
                 
                 
-                "e2e/TC01-loadPollutionStats.js",
+               "e2e/TC01-loadPollutionStats.js",
                 
                 "e2e/TC01-loadYouthUnemploymentStats.js",
                 
  
-                "e2e/TC02-createPollutionStats.js",
+               "e2e/TC02-createPollutionStats.js",
                 
                 "e2e/TC02-createYouthUnemploymentStats.js",
 
