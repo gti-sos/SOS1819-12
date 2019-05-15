a596c56f27bdf8f26a84389f0221c02a1ed88af3
sos1819ajm <alberto_jimenez_1996@hotmail.com>
2019-05-15 00:11:23 +0000

    ok

--- tests/tests.js
+++ tests/tests.js
@@ -4,24 +4,25 @@ exports.config = {
     chromeOnly: true,
     specs: [
         
-                "e2e/TC01-loadPollutionStats.js",
+                //"e2e/TC01-loadPollutionStats.js",
                 
-                "e2e/TC01-loadYouthUnemploymentStats.js",
+                //"e2e/TC01-loadYouthUnemploymentStats.js",
                 
-                "e2e/TC01-loadLifeExpectaSncyStats.js",
+                "e2e/TC01-loadLifeExpectancyStats.js",
                     
  
-                "e2e/TC02-createPollutionStats.js",
+                //"e2e/TC02-createPollutionStats.js",
                 
                 "e2e/TC02-createLifeExpectancyStats.js",
                 
-                "e2e/TC02-createYouthUnemploymentStats.js",
+                //"e2e/TC02-createYouthUnemploymentStats.js",
 
                     
-                "e2e/TC03-removePollutionStats.js",
+                //"e2e/TC03-removePollutionStats.js",
                 
-                "e2e/TC03-removeYouthUnemploymentStats.js",
+                //"e2e/TC03-removeYouthUnemploymentStats.js",
                 
-                "e2e/TC03-removeLifeExpectancyStats.js"
-                ]
+                //"e2e/TC03-removeLifeExpectancyStats.js"
+            
+            ]
 };
