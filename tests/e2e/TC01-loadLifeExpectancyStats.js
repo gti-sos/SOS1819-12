49724cfbd8937254677c6a52f1ae50a62616919f
sos1819ajm <alberto_jimenez_1996@hotmail.com>
2019-05-15 00:30:30 +0000

    ok

--- tests/e2e/TC01-loadLifeExpectancyStats.js
+++ tests/e2e/TC01-loadLifeExpectancyStats.js
@@ -2,7 +2,7 @@ describe("Check if life expectancy data is loaded: ", function(){
         
         it("Life expectancy list should show a bunch of data.", function(){
                 
-                browser.get("https://sos1819-12.herokuapp.com/#!//ui/v1/life-expectancy-stats");
+                browser.get("https://sos1819-12.herokuapp.com/#!/ui/v1/life-expectancy-stats");
                 
                 var les = element.all(by.repeater("stat in life_expectancy_stats"));
                 expect(les.count()).toBeGreaterThan(0);
