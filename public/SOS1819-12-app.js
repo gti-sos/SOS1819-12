4976ac1f8fbcbc001961719a7a2aa11f53b7131b
sos1819ajm <alberto_jimenez_1996@hotmail.com>
2019-05-15 07:28:22 +0000

    okEstadoMal

--- public/SOS1819-12-app.js
+++ public/SOS1819-12-app.js
@@ -12,28 +12,18 @@ angular.module("SOS181912App",["ngRoute"])
             .when("/ui/v1/life-expectancy-stats/edit/:country/:year",{
                 controller: "EditLESCtrl",
                 templateUrl: "frontend/v1/edit-LES.html"
-<<<<<<< HEAD
-            }).when("/ui/v1/youth-unemployment-stats",{
-                controller: "ListyCtrl",
+<<<<<<< HE            }).when("/ui/v1/youth-unemployment-stats"                controller: "ListyCtrl",
                 templateUrl: "youthApp/v1/listy.html"
             }).when("/ui/v1/youth-unemployment-stats/edit/:country/:year",{
                 controller: "EdityCtrl",
                 templateUrl: "youthApp/v1/edity.html"
-            });
-=======
-            })
-              .when("/ui/v1/pollution-stats",{
+            }).when("/ui/v1/pollution-stats",{
                 controller: "ListCtrl",
                 templateUrl: "frontendPollution/v1/list.html"
             })
             .when("/ui/v1/pollution-stats/edit/:country/:year",{
                 controller: "EditCtrl",
                 templateUrl: "frontend/v1/edit.html"
-            }) 
+            });
             
-            
-            
-            ;
->>>>>>> f29d1d5e6f8364cb56c97bc559664db457436ec3
-    });
-console.log("SOS1819-12 App initialized!");
+>>>>>>> f29d1d5e6f8364cb56c97bc5console.log("SOS1819-12 App initialized!");
