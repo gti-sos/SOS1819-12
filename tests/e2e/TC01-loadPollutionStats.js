7a272ed8780199b476a84cbb258e7863d1e39ad1
sos1819aen <antonioescobar070@gmail.com>
2019-05-22 08:08:07 +0000

    bien

--- tests/e2e/TC01-loadPollutionStats.js
+++ tests/e2e/TC01-loadPollutionStats.js
@@ -2,7 +2,7 @@ describe("Data is Loaded: ", function(){
         
         it("List should a bunch of data.", function(){
                 
-                browser.get("https://sos1819-12.herokuapp.com/ui/v1/pollution-stats/#!/");
+                browser.get("https://sos1819-12.herokuapp.com/#!/ui/v1/pollution-stats");
                 
                 var pollution = element.all(by.repeater("stat in pollutionStats"));
                 expect(pollution.count()).toBeGreaterThan(0);
